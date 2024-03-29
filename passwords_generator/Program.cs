9269275a9f302fc2f1ca260fb8bfe891fbc73597
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-03-29 11:19:41 +0100

    Ajoutez des fichiers projet.

---
+++ passwords_generator/Program.cs
@@ -0,0 +1,69 @@
+﻿
+namespace passwords_generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NB_PASSWORDS = 10;
+
+            // Demander la longueur du mot de passe (Demander nombre) int longueur_mot_de_passe = ...
+            int passwordLength = Tools.RequestPositiveNonNullNumber("Longueur du mot de passe : ");
+
+            Console.WriteLine();
+            int choiceAlphabet = Tools.RequestNumberBetween("Vous voulez un mot de passe avec :\n" +
+                "1 - Uniquement des caractères en minuscules\n" +
+                "2 - Des caractère en minuscules et majuscules\n" +
+                "3 - Des caractères et des chiffres\n" +
+                "4 - Caractères, chiffres et caractères spéciaux\n" +
+                "Votre choix : ", 1, 4);
+
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string uppercase = lowercase.ToUpper();
+            string numbers = "0123456789";
+            string specialCharacters = "&#@$%!?";
+            string alphabet = "";
+
+            Random rand = new Random();
+
+            Console.WriteLine();
+            Console.WriteLine("Voici vos mots de passes :\n");
+
+            switch (choiceAlphabet)
+            {
+                case 1:
+                    alphabet = lowercase;
+                    break;
+                case 2:
+                    alphabet = lowercase + uppercase;
+                    break;
+                case 3:
+                    alphabet = lowercase + uppercase + numbers;
+                    break;
+                case 4:
+                    alphabet = lowercase + uppercase + numbers + specialCharacters;
+                    break;
+                default:
+                    Console.WriteLine("ERREUR : Qu'est-ce que vous avez foutu bordel !");
+                    break;
+            }
+
+            int alphabetLength = alphabet.Length;
+            string password;
+
+            for (int i = 0; i < NB_PASSWORDS; i++)
+            {
+                password = "";
+
+                for (int j = 0; j < passwordLength; j++)
+                {
+                    int index = rand.Next(0, alphabetLength);
+                    password += alphabet[index];
+                }
+
+                Console.WriteLine($"{password}");
+            }
+
+        }
+    }
+}
---
+++ passwords_generator/Tools.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passwords_generator
+{
+    internal class Tools
+    {
+        public static int RequestPositiveNonNullNumber(string question)
+        {
+            return RequestNumberBetween(question, 1, int.MaxValue, "ERREUR : Le nombre doit être positif et non null.");
+        }
+
+        public static int RequestNumberBetween(string question, int min, int max, string customErrorMessage = null)
+        {
+            int number = RequestNumber(question);
+
+            if (number >= min && number <= max)
+            {
+                return number;
+            }
+
+            if (customErrorMessage == null)
+            {
+                Console.WriteLine($"ERREUR : Le nombre doit être compris entre {min} et {max}.");
+            }
+            else
+            {
+                Console.WriteLine(customErrorMessage);
+            }
+            Console.WriteLine();
+
+            return RequestNumberBetween(question, min, max, customErrorMessage); // Fonction récursive
+        }
+
+        public static int RequestNumber(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                try
+                {
+                    int responseInt = int.Parse(Console.ReadLine());
+                    return responseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Veuillez entrer un nombre valide.");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
